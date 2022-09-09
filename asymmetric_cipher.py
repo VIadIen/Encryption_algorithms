f5691945578fae3888edaa5b761834e8ffd18e22
Viadien <kashkarov.00@bk.ru>
2022-09-10 01:40:39 +0300

    Clear asymmetric cipher

--- asymmetric_cipher.py
+++
@@ -1,8 +0,0 @@
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-            "V", "W", "X", "Y", "Z", " ", "\n", ",", "'", ".", "!", "?"]
-string = [str(alphabet.index(i)) for i in input().upper()]
-code = ''
-for i in string:
-    code += i
-print(code)
-print(len(alphabet))
