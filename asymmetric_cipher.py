8a2b90941cd37e6fe1b6d0298e6604e2bfd17d6d
Viadien <kashkarov.00@bk.ru>
2022-09-19 15:41:17 +0300

    Revert "Russian language added"
    This reverts commit 03ffe31ab9bb5d0912766c0b1d6da2e6800bcde6.

--- asymmetric_cipher.py
+++ asymmetric_cipher.py
@@ -4,10 +4,10 @@ from random import randint as rand
 def cryption_message(message, key):
     cipher_message = ''
     for i in message:
-        k = rand(2, 1107)
-        y = 277 ** key % 1109
-        a = 277 ** k % 1109
-        b = y ** k * ord(i) % 1109
+        k = rand(2, 347)
+        y = 58 ** key % 349
+        a = 58 ** k % 349
+        b = y ** k * ord(i) % 349
         cipher_message += str(a) + ' ' + str(b) + ' '
     return cipher_message
 
@@ -17,7 +17,7 @@ def decryption_message(cipher, key):
     for j in range(0, len(cipher) - 1, 2):
         a = int(cipher[j])
         b = int(cipher[j + 1])
-        message += chr((b * a ** (1109 - 1 - key)) % 1109)
+        message += chr((b * a ** (349 - 1 - key)) % 349)
     return message
 
 
